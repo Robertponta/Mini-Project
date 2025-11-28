1b0d112ae9c6efe43d7f11534390fe8c065e6a1b
Renato Ikeda Bressan <renatoikeda98@gmail.com>
2025-11-28 11:59:51 -0300

    Implementando as funções de divisão, máximo, absoluto e menor ou igual

--- Codes Folder/Miniproject.c
+++ Codes Folder/Miniproject.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
 
+void division(int a, int b, double *div) {
+    if (b == 0) div = NULL;
+    *div = a / b;
+}
+
+void maximum(int a, int b, int *max) {
+    if (a >= b) *max = a;
+    else *max = b;
+}
+
+void absolute(int x, int *abs) {
+    if (x < 0) *abs = -x;
+    else *abs = x;
+}
+
+void menorOuIgual(int a, int b, int *menorouigual) {
+    if (a <= b) *menorouigual = 1;
+    else *menorouigual = 0;
+} 
+
 int main(){
 
 
