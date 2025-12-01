cb160e5592b078d3de67789d1e8d1054befafba6
Robert Taveira <robertfranciscotav@gmail.com>
2025-11-30 21:15:41 -0300

    Implementação das funções 6,26,22,28 e declaração dessas e demais funções no início do código

--- Codes Folder/Miniproject.c
+++ Codes Folder/Miniproject.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 
+void division(int a, int b, double *div);
+void maximum(int a, int b, int *max);
+void absolute(int x, int *abs);
+void menorOuIgual(int a, int b, int *menorouigual);
+void menorQue(int a,int b,int *menorque);
+void potencia(int base,int expoente,long long int *resultado);
+void notAnd(int a,int b,int *notand);
+void notOr(int a,int b,int *notor);
+
+int main(){
+
+
+
+  
+
+  return 0;
+}
+
+
 void division(int a, int b, double *div) {
     if (b == 0) div = NULL;
     *div = a / b;
@@ -20,11 +39,21 @@ void menorOuIgual(int a, int b, int *menorouigual) {
     else *menorouigual = 0;
 } 
 
-int main(){
+void menorQue(int a,int b,int *menorque){
+    *menorque=(a<b)? 1:0;
+}
 
+void potencia(int base,int expoente,long long int *resultado){
+*resultado=1;
+    for(int i=0;i<expoente;i++){
+        *resultado*=base;
+    }
+}
 
+void notAnd(int a,int b,int *notand){
+    *notand=(a&&b)? 0:1;
+}
 
-  
-
-  return 0;
+void notOr(int a,int b,int *notor){
+    *notor=(a||b)? 0:1;
 }
