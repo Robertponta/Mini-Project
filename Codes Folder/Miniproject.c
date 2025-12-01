2b8657da996cd3419f9d281e6ac3baa308cfd140
TRIGOLS <161969145+TRIGOLS@users.noreply.github.com>
2025-11-30 21:25:00 -0300

    Alterações corretas dispostas na main
    Implementação das funções diretamente na main disponiveis para teste e possivel merge

--- Codes Folder/Miniproject.c
+++ Codes Folder/Miniproject.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+//Renato
 void division(int a, int b, double *div) {
     if (b == 0) div = NULL;
     *div = a / b;
@@ -19,6 +20,32 @@ void menorOuIgual(int a, int b, int *menorouigual) {
     if (a <= b) *menorouigual = 1;
     else *menorouigual = 0;
 } 
+
+//Paulo
+void fxor(int a, int b, int *r) {
+    *r = a ^ b;
+}
+
+void fsoma(long long a, long long b, long long *r) {
+    if (a > b) {
+        long long t = a;
+        a = b;
+        b = t;
+    }
+    long long n = b - a + 1;
+    *r = (n * (a + b)) / 2;
+}
+
+void fig(int a, int b, int *r) {
+    *r = (a == b) ? 1 : 0;
+}
+
+void fand(int a, int b, int *r) {
+    int x = (a != 0);
+    int y = (b != 0);
+    *r = (x && y) ? 1 : 0;
+}
+
 
 int main(){
 
